8820781c58717d4cf4708e33308af681697e848b
RudahLopes <78888049+RudahLopes@users.noreply.github.com>
2021-06-01 22:40:31 -0300

    Delete lala.js

--- lala.js
+++
@@ -1,8 +0,0 @@
-var pais = 'brasil'
-if (pais.toLowerCase() != 'brasil') {
-    console.log('Tu é estrangeiro!')
-    
-} 
-else {
-    console.log('. Isso significa que tu é brasileiro, meu irmão. Dá cá um abraço!')
-}
